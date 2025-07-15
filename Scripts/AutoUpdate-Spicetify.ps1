63cf8b4314d8f2489c03c4009544ff1bc98c55f1
Sky Williamson <96271755+SlawSimulation@users.noreply.github.com>
2025-07-15 21:59:09 +0100

    AutoUpdate-Spicetify.ps1

---
+++ Scripts/AutoUpdate-Spicetify.ps1
@@ -0,0 +1,3 @@
+# AutoUpdate‑Spicetify.ps1
+$ErrorActionPreference = 'Stop'          # halt if anything fails
+spicetify update                         # pulls & applies only when a new build exists
